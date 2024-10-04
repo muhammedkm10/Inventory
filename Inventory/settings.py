81000be8eff7d5e7b2fb25a45cde74fbdcd32d18
muhammed <muhammedmamu2906@gmail.com>
2024-10-04 19:14:38 +0530

    redis caching implemented

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -158,3 +158,16 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=4),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+
+# caching
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1', 
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
--- ProductApp/views.py
+++ ProductApp/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from .serializer import ProductSerializer
 from rest_framework import status
 from .models import Products
+from django.core.cache import cache
 
 # product management view
 class ProductManagment(APIView):
@@ -20,16 +21,30 @@ class ProductManagment(APIView):
     
     # get a product or all produts
     def get(self,request,item_id = None):
+        
         if item_id:
+            cache_key = f"item_{item_id}"
+            item_data = cache.get(cache_key)
+            if item_data is not None:
+                # If item data is found in cache returning it
+                return Response(item_data, status=status.HTTP_200_OK)
             try:
                 item = Products.objects.get(id = item_id)
                 serializer = ProductSerializer(item,many=False)
+                cache.set(cache_key, serializer.data, timeout=3600)
                 return Response(serializer.data,status=status.HTTP_302_FOUND)
             except:
                 return Response({"error":"The item is not present"},status=status.HTTP_404_NOT_FOUND)
+            
+        # fetching all data from the cache
+        cache_key = 'all_items'
+        items = cache.get(cache_key)
+        if items is not None:
+            return Response(items,status=status.HTTP_200_OK)
         items = Products.objects.all()
         serializer = ProductSerializer(items,many=True)
         if serializer.data:
+            cache.set(cache_key,serializer.data,timeout=3600)
             return Response(serializer.data,status=status.HTTP_302_FOUND)
     
     # updating the details
